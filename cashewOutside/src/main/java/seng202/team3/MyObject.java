20c26f861562e28b77159ce43edc684ad4432ac0
ajo110 <ajo110@uclive.ac.nz>
2019-08-01 17:50:27 +1200

    initial commit with Maven

---
+++ cashewOutside/src/main/java/seng202/team3/MyObject.java
@@ -0,0 +1,15 @@
+package seng202.team3;
+
+public class MyObject {
+
+    private String name = "The name of my object";
+    private int quantity = 11;
+
+    public void MyObject() {
+        System.out.println(name + quantity);
+    }
+
+    public static void main(String[] args){
+
+    }
+}
