88490c6c6f657df888dc77ad048930134070d24c
Alyssa <ajo110@uclive.ac.nz>
2019-08-01 18:14:29 +1200

    Initial commit

---
+++ cashew_outside/src/main/java/seng202/team3/MyObject.java
@@ -0,0 +1,15 @@
+package seng202.team3;
+
+public class MyObject {
+    public String name = "The name of my object";
+    public int quantity = 11;
+
+    public String MyObject() {
+
+        return name + quantity;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
