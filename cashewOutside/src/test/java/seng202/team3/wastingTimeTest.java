107ab35cfcd9fa54db51438a41008b714df38552
Alyssa <ajo110@uclive.ac.nz>
2019-08-10 23:18:17 +1200

    Creating junit test case for the signing off business

---
+++ cashewOutside/src/test/java/seng202/team3/wastingTimeTest.java
@@ -0,0 +1,22 @@
+package seng202.team3;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class wastingTimeTest
+    extends TestCase
+{
+
+    /**
+     * Testing myPatience class
+     * Created by A Josephs
+     */
+    public void testingPatience() {
+    assertEquals(333, 999/3);
+    }
+
+}
