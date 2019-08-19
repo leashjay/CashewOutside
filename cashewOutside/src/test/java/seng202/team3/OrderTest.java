d75559db7d45cdb8d54ba3656054c0748ce021dc
Hunter Craig <hjc94@uclive.ac.nz>
2019-08-20 09:11:34 +1200

    Uncommented OrderTest, works with new project structure

--- cashewOutside/src/test/java/seng202/team3/OrderTest.java
+++ cashewOutside/src/test/java/seng202/team3/OrderTest.java
@@ -1,12 +1,12 @@
-//package test.java.seng202.team3;
-//
-//import model.Order;
-//import org.junit.Test;
-//
-//public class OrderTest {
-//
-//    @Test
-//    public void dummyTest() {
-//        Order order = new Order();
-//    }
-//}
+package java.seng202.team3;
+
+import model.Order;
+import org.junit.Test;
+
+public class OrderTest {
+
+    @Test
+    public void dummyTest() {
+        Order order = new Order();
+    }
+}
