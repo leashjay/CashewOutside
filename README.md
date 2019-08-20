d4849d9d052659d6ffe6ff2ea490a5152f26a714
Jack Steel <jes143@uclive.ac.nz>
2019-08-21 10:55:32 +1200

    Remove all files

--- README.md
+++
@@ -1,2 +0,0 @@
-An application for managing a pop-up point of food sale. 
-Team 3s SENG202 group project.
--- cashewOutside/src/main/java/util/PhoneType.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-/**
- * Simple enum to illustrate XML attribute parsingin SAX example
- */
-public enum PhoneType {
-    MOBILE, WORK, HOME, UNKNOWN
-}
