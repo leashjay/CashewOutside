c20af286c07c425039d150fbbae78c8a10ec19f0
Alyssa <ajo110@uclive.ac.nz>
2019-08-29 19:34:16 +1200

    Adding controller and view classes for bug checking with partner. I think its a module that needs to be in the pom but could be wrong. feel free to delete the files out if they jam you up

---
+++ src/main/java/seng202/team3/controller/mainController.java
@@ -0,0 +1,15 @@
+package seng202.team3.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class mainController {
+    //@FXML
+    //private Text pressedCountText;
+
+    //private int count = 0;
+
+    //public void countButtonPressed() {
+    //    count += 1;
+    //    pressedCountText.setText(String.format("Count: %s", count));
+}
