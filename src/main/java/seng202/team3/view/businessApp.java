8539774fa6e3663e41f6701ab3c4b96fcfacdfc3
Alyssa <youremail@uclive.ac.nz>
2019-09-04 16:50:53 +1200

    Fixed system to allow javafx to run. Have css files for button and tab style

--- src/main/java/seng202/team3/view/businessApp.java
+++ src/main/java/seng202/team3/view/businessApp.java
@@ -6,20 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 import java.io.IOException;
 
 public class businessApp extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-        primaryStage.setTitle("Main Business Landing Page");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        primaryStage.setTitle("Main Business Landing Page");
+        primaryStage.setScene(new Scene(root, 800, 300));
+        primaryStage.show();
     }
 }
---
+++ src/main/java/seng202/team3/view/main.java
@@ -0,0 +1,8 @@
+package seng202.team3.view;
+
+public class main {
+
+    public static void main(String[] args) {
+        businessApp.main(args);
+    }
+}
