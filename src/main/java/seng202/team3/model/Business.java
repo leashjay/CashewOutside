03855477da9fbfade834ab3785be7d1656620d91
chengyi19 <cyk19@uclive.ac.nz>
2019-09-05 16:40:41 +1200

    Populate truck and business class

--- src/main/java/seng202/team3/model/Business.java
+++ src/main/java/seng202/team3/model/Business.java
@@ -1,15 +1,35 @@
 package seng202.team3.model;
 
-import java.util.List;
+import seng202.team3.parsing.MenuLoader;
+import seng202.team3.parsing.SuppliersLoader;
+
+import javax.xml.bind.JAXBException;
 
 /**
  * Main class for the business. Keeps track of the model classes (suppliers
  * etc.) that we have as well as performing major functions.
  */
 public class Business {
-    List<Supplier> suppliers;
+    private Truck thisTruck;
+    private SupplierHandler supplierManager;
+    private Menu menuManager;
+    private SalesHandler salesManager;
 
-    public void setSuppliers(List<Supplier> s) {
-        suppliers = s;
+    public void Business(String ingredientsData, String menuData, String supplierData) throws JAXBException {
+        thisTruck = new Truck();
     }
+
+
+    public void createSupplierManager(String fileName) throws JAXBException {
+        SuppliersLoader supplierLoad = new SuppliersLoader();
+        supplierManager = supplierLoad.loadSuppliersData(fileName);
+    }
+
+    public void createMenuManager(String fileName) throws JAXBException {
+        MenuLoader menuLoad = new MenuLoader();
+        menuManager = menuLoad.loadMenuData(fileName);
+    }
+
+   
+
 }
