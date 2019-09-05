c284d2c389fa0df45006f75bfc6303435977a59b
Alyssa <youremail@uclive.ac.nz>
2019-09-05 13:11:20 +1200

    Removed error squiggle by adjusting case of BusinessApp

--- src/main/java/seng202/team3/controller/mainController.java
+++ src/main/java/seng202/team3/controller/mainController.java
@@ -1,14 +1,5 @@
 package seng202.team3.controller;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import seng202.team3.view.businessApp;
-
-import java.io.IOException;
-
 public class mainController {
 
 //    public void salesButtonPressed() throws IOException {
