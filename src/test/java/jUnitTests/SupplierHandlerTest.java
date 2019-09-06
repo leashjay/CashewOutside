c90a986946d29f3abbfad43bd8aba1e575d64597
Andrew Sturman <ads92@uclive.ac.nz>
2019-09-06 14:36:56 +1200

    Added more tests to the SupplierHandler class, modified remove supplier method to accept ID rather than supplier as parameter

--- src/test/java/jUnitTests/SupplierHandlerTest.java
+++ src/test/java/jUnitTests/SupplierHandlerTest.java
@@ -10,6 +10,8 @@ import seng202.team3.util.UnitType;
 import java.util.Date;
 import java.util.HashMap;
 
+import static junit.framework.TestCase.*;
+
 public class SupplierHandlerTest {
 
     private Ingredient beans;
@@ -17,6 +19,9 @@ public class SupplierHandlerTest {
     private Ingredient brocolli;
     private HashMap<String, Supplier> suppliers;
     private Inventory inventory;
+    private Supplier countdown;
+    private Supplier mootsMeatMarket;
+    private Supplier paknsave;
 
     @Before
     public void setup() {
@@ -27,23 +32,64 @@ public class SupplierHandlerTest {
         carrots = new Ingredient("3", "Carrots", UnitType.COUNT, ThreeValueLogic.YES, ThreeValueLogic.YES,
                 ThreeValueLogic.YES, 10);
         inventory = new Inventory();
-        suppliers = new HashMap<String, Supplier>();
+        suppliers = new HashMap<>();
 
+         countdown = new Supplier("1", "Countdown", "20 Church Corner", PhoneType.WORK, "9593999", "countdown@gmail.com", "www.countdown.com");
+         mootsMeatMarket = new Supplier("2", "Moots Meat Market", "52 Riccarton Road", PhoneType.WORK, "9597999", "moots@gmail.com", "www.moots.com");
+         paknsave = new Supplier("3", "paknsave", "73 Riccarton Road", PhoneType.HOME, "9599989", "paknsave@gmail.com", "www.paknsave.com");
     }
 
 
     @Test
     public void orderFromSupplierTest() {
-        Supplier supplier = new Supplier("1", "Countdown", "20 Riccarton Road", PhoneType.WORK, "9599999", "countdown@gmail.com", "www.countdown.com");
-        suppliers.put(supplier.getSid(), supplier);
+        suppliers.put(countdown.getSid(), countdown);
         SupplierHandler supplierHandler = new SupplierHandler("Array containing one supplier", suppliers);
         HashMap<Ingredient, Float> orderHashMap = new HashMap<>();
         orderHashMap.put(beans, 20f);
         orderHashMap.put(carrots, 40f);
         orderHashMap.put(brocolli, 50f);
-        SupplierOrder supplierOrder = new SupplierOrder(supplier, orderHashMap, new Date());
+        SupplierOrder supplierOrder = new SupplierOrder(countdown, orderHashMap, new Date());
         supplierHandler.orderFromSupplier(supplierOrder, inventory);
     }
+
+    //TODO agree on whether to use a hashmap or a list for suppliers
+    @Test
+    public void modifySuppliersTest(){
+        HashMap<String, Supplier> suppliers = new HashMap<>();
+        SupplierHandler supplierHandler = new SupplierHandler(suppliers);
+        supplierHandler.addSupplier(countdown);
+        supplierHandler.addSupplier(mootsMeatMarket);
+        assertEquals(supplierHandler.getSuppliers().size(), 2);
+        supplierHandler.removeSupplier(mootsMeatMarket.getSid());
+        assertFalse(supplierHandler.getSuppliers().containsKey(mootsMeatMarket));
+        System.out.println(supplierHandler.getSuppliers().size());
+        assertEquals(supplierHandler.getSuppliers().size(), 1);
+        supplierHandler.addSupplier(paknsave);
+        assertEquals(2, supplierHandler.getSuppliers().size());
+    }
+
+    @Test
+    public void replaceSuppliersTest(){
+        HashMap<String, Supplier> oldSuppliers = new HashMap<>();
+        SupplierHandler supplierHandler = new SupplierHandler(suppliers);
+        supplierHandler.addSupplier(countdown);
+        supplierHandler.addSupplier(mootsMeatMarket);
+
+        HashMap<String, Supplier> newSuppliers = new HashMap<>();
+        newSuppliers.put(paknsave.getSid(), paknsave);
+        supplierHandler.setSuppliers(newSuppliers);
+
+        assertEquals(supplierHandler.getSuppliers().size(), 1);
+        assertTrue(supplierHandler.getSuppliers().containsValue(paknsave));
+        assertFalse(supplierHandler.getSuppliers().containsValue(countdown));
+        assertFalse(supplierHandler.getSuppliers().containsValue(mootsMeatMarket));
+
+
+
+    }
+
+
+
 
 
 }
