cd13adede1ee337f20031b84b935e31cdce254d7
Andrew Sturman <ads92@uclive.ac.nz>
2019-09-08 15:41:38 +1200

    Starting to implement navigation from main screen to the 3 windows

--- src/main/java/seng202/team3/controller/mainController.java
+++ src/main/java/seng202/team3/controller/mainController.java
@@ -1,6 +1,26 @@
 package seng202.team3.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
+import java.io.IOException;
+
 public class mainController {
 
+    public void openManagement(){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("management.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 }
