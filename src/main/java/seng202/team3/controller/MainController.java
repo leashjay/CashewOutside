6d28b7b4885adf87e665528866524c03b194786e
Andrew Sturman <ads92@uclive.ac.nz>
2019-09-19 19:23:29 +1200

    Added main business logic for accessing the business class with Hunter and Cheng.

--- src/main/java/seng202/team3/controller/MainController.java
+++ src/main/java/seng202/team3/controller/MainController.java
@@ -6,15 +6,6 @@ import seng202.team3.view.BusinessApp;
 import java.io.IOException;
 
 public class MainController {
-    private String ingredientsXML = "./resources/data/Ingredients.xml";
-    private String menuXML = "./resources/data/SampleMenu.xml";
-    private String suppliersXML = "./resources/data/Suppliers.xml";
-    private Business business;
-
-
-    public void initialize() throws Exception {
-        this.business = new Business(ingredientsXML, menuXML, suppliersXML);
-    }
 
     public void salesButtonPressed() throws IOException {
         System.out.println("Sales button pressed");
@@ -31,7 +22,4 @@ public class MainController {
         BusinessApp.loadKitchenPage();
     }
 
-    public Business getBusiness() {
-        return business;
-    }
 }
--- src/main/java/seng202/team3/controller/ManuallyAddSupplierController.java
+++ src/main/java/seng202/team3/controller/ManuallyAddSupplierController.java
@@ -3,9 +3,16 @@ package seng202.team3.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import seng202.team3.model.Business;
+import seng202.team3.model.Supplier;
+import seng202.team3.model.SupplierHandler;
 import seng202.team3.util.PhoneType;
+import seng202.team3.util.ThreeValueLogic;
+import seng202.team3.view.BusinessApp;
 
 public class ManuallyAddSupplierController {
+
+    SupplierHandler supplierHandler = BusinessApp.getBusiness().getSupplierHandler();
 
     @FXML
     TextField idTextField;
@@ -33,6 +40,8 @@ public class ManuallyAddSupplierController {
         phoneTypeChoiceBox.getItems().add(PhoneType.MOBILE);
         phoneTypeChoiceBox.getItems().add(PhoneType.WORK);
         phoneTypeChoiceBox.getItems().add(PhoneType.UNKNOWN);
+
+        phoneTypeChoiceBox.setValue(PhoneType.UNKNOWN);
     }
 
     //TODO Add input validation on this form
@@ -42,8 +51,10 @@ public class ManuallyAddSupplierController {
         String address = addressTextField.getText();
         String email = emailTextField.getText();
         String phoneNumber = phoneTextField.getText();
+        PhoneType phoneType = phoneTypeChoiceBox.getValue();
         String url = urlTextField.getText();
 
+        //Supplier newSupplier =
         System.out.println("Name is " + nameTextField.getText()); //+ nameTextField.getText());
 
     }
