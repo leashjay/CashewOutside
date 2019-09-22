fd8c96ca7874e8e94f4864a8520d6ae7aa95bb5f
Andrew Sturman <ads92@uclive.ac.nz>
2019-09-22 16:13:10 +1200

    Finished input validation for both forms. Moved Input validation logic into its own helper class.

--- src/main/java/seng202/team3/controller/ManuallyAddIngredientController.java
+++ src/main/java/seng202/team3/controller/ManuallyAddIngredientController.java
@@ -74,56 +74,21 @@ public class ManuallyAddIngredientController {
     public boolean checkForErrors(){
         boolean hasError = false;
 
-        if(checkEmpty(idTextField, idErrorText)){
+        if(InputValidationHelper.checkEmpty(idTextField, idErrorText)){
             hasError = true;
         }
-        if(checkEmpty(nameTextField, nameErrorText)){
+        if(InputValidationHelper.checkEmpty(nameTextField, nameErrorText)){
             hasError = true;
         }
 
-        if(!isValidFloat(costTextField, costErrorText)){
+        if(!InputValidationHelper.isValidFloat(costTextField, costErrorText)){
             hasError = true;
         }
 
         return hasError;
     }
 
-    /**
-     * Method that true empty if a textfield contains no text
-     * @param textField the textfield to be checked
-     * @param text the error text to be displayed if the textfield is empty
-     * @return whether or not the textfield is empty
-     */
-    private boolean checkEmpty(TextField textField, Text text){
-        boolean hasError = false;
-        if(textField.getText().isEmpty()){
-            text.setVisible(true);
-            hasError = true;
-        } else {
-            text.setVisible(false);
-        }
-        return hasError;
-    }
 
-    /**
-     * Checks whether a textfield contains a valid float
-     * @param textField the textfield that is being checked
-     * @param errorText the error text to be displayed if the textfield does not contain a valid float
-     * @return a boolean showing whether or not the textfield has a valid float.
-     */
-    private boolean isValidFloat(TextField textField, Text errorText){
-        float temp = 0 ;
-        boolean isFloat = true;
-        try {
-            temp = Float.parseFloat(textField.getText());
-            errorText.setVisible(false);
-        } catch (NumberFormatException ex) {
-            isFloat = false;
-            errorText.setVisible(true);
-        }
-
-        return isFloat;
-    }
 
 
 //    private boolean containsInput(){
