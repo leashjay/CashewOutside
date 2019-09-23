43cce404d3d5112bfe07b65c2d198c39a7c2aa4f
Jack Walsh <jwa227@uclive.ac.nz>
2019-09-23 23:59:47 +1200

    Merge branch 'master' of C:\Users\walsh\Documents\cashewOutside with conflicts.

--- src/main/java/seng202/team3/model/MenuList.java
+++ src/main/java/seng202/team3/model/MenuList.java
@@ -9,12 +9,14 @@ import javax.xml.bind.annotation.XmlAttribute;
 public class MenuList {
 
     private ArrayList<Menu> menuArrayList;
-    private ArrayList<Menu> activeMenus;
-    private ArrayList<Menu> winterMenus;
-    private ArrayList<Menu> summerMenus;
-    private ArrayList<Menu> festivalMenus;
-    @XmlAttribute(name = "type")
-    private MenuType type;
+    private ArrayList<Menu> activeMenus = new ArrayList<>();
+    private ArrayList<Menu> winterMenus = new ArrayList<>();
+    private ArrayList<Menu> summerMenus = new ArrayList<>();
+    private ArrayList<Menu> festivalMenus = new ArrayList<>();
+
+    public MenuList(ArrayList menuArrayList) {
+        this.menuArrayList = menuArrayList;
+    }
 
     public void addMenu(Menu menu) {
         if (!menuArrayList.contains(menu)) {
@@ -42,6 +44,7 @@ public class MenuList {
 
 
     public ArrayList<Menu> getWinterMenus() {
+        winterMenus.clear();
         for (int i = 0; i < menuArrayList.size(); i++) {
             Menu current = menuArrayList.get(i);
             if (current.getMenuType() == MenuType.WINTER) {
@@ -52,6 +55,7 @@ public class MenuList {
     }
 
     public ArrayList<Menu> getSummerMenus() {
+        summerMenus.clear();
         for (int i = 0; i < menuArrayList.size(); i++) {
             Menu current = menuArrayList.get(i);
             if (current.getMenuType() == MenuType.SUMMER) {
@@ -62,12 +66,16 @@ public class MenuList {
     }
 
     public ArrayList<Menu> getFestivalMenus() {
+        festivalMenus.clear();
         for (int i = 0; i < menuArrayList.size(); i++) {
             Menu current = menuArrayList.get(i);
+            System.out.println(current);
             if (current.getMenuType() == MenuType.FESTIVAL) {
+                System.out.println("we in\n");
                 festivalMenus.add(current);
             }
         }
+        System.out.println("done\n=====================");
         return festivalMenus;
     }
 }
