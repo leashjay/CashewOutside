365cacfcdcbd38cb605eb18327ead1e4a4ba044f
Andrew Sturman <ads92@uclive.ac.nz>
2019-09-23 22:05:18 +1200

    Removed print statements on buttons. Fixed link to the css file in the kitchen fxml sheet.

--- src/main/java/seng202/team3/controller/MainController.java
+++ src/main/java/seng202/team3/controller/MainController.java
@@ -8,17 +8,14 @@ import java.io.IOException;
 public class MainController {
 
     public void salesButtonPressed() throws IOException {
-        System.out.println("Sales button pressed");
         BusinessApp.loadSalesPage();
     }
 
     public void managementButtonPressed() throws IOException {
-        System.out.println("ManagementController button pressed");
         BusinessApp.loadManagementPage();
     }
 
     public void kitchenButtonPressed() throws IOException {
-        System.out.println("Kitchen button pressed");
         BusinessApp.loadKitchenPage();
     }
 
