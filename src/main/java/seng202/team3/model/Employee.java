792d3a022bd1ffda0ae64145209711de105c8b6e
Andrew Sturman <ads92@uclive.ac.nz>
2019-09-29 18:11:49 +1300

    Created class to store data about a single employee and a manager class to hold information about all the employees in the business. Username and password currently stored in plaintext.

---
+++ src/main/java/seng202/team3/model/Employee.java
@@ -0,0 +1,74 @@
+package seng202.team3.model;
+
+/**
+ * Class holding data about a single employee of the foodtruck
+ */
+public class Employee {
+
+    /**
+     * Gets the username of the employee
+     * @return the username of the employee
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Sets the username of the employee
+     * @param userName the new username of the employee
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * Gets the password of the employee
+     * @return the password of the employee
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets the password of the employee
+     * @param password the new password of the employee
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Returns if the user has admin rights
+     * @return true if the user has admin rights
+     */
+    public boolean hasAdminRights() {
+        return hasAdminRights;
+    }
+
+    /**
+     * Sets whether a user has admin rights
+     * @param hasAdminRights true if the user should have admin rights
+     */
+    public void setHasAdminRights(boolean hasAdminRights) {
+        this.hasAdminRights = hasAdminRights;
+    }
+
+    private String userName;
+    private String password;
+    private boolean hasAdminRights;
+
+    /**
+     * Constructor for an instance of an employee
+     * @param userName The employees username
+     * @param password The employees password
+     * @param hasAdminRights a boolean showing whether the employee has administrative rights
+     */
+    public Employee(String userName, String password, boolean hasAdminRights){
+        this.userName = userName;
+        this.password = password;
+        this.hasAdminRights = hasAdminRights;
+    }
+
+
+
+}
---
+++ src/main/java/seng202/team3/model/EmployeeHandler.java
@@ -0,0 +1,38 @@
+package seng202.team3.model;
+
+import java.util.ArrayList;
+
+/**
+ * Class to hold information about all the employees in the food truck
+ */
+public class EmployeeHandler {
+    private ArrayList<Employee> employees;
+
+    /**
+     * Adds an employee to the list of employees
+     * @param employee the new employee to be added
+     */
+    public void addEmployee(Employee employee){
+        employees.add(employee);
+    }
+
+    /**
+     * Removes an employee from the list of employees
+     * @param employee the employee to be removed.
+     */
+    public void removeEmployee(Employee employee){
+        employees.remove(employee);
+    }
+
+    /**
+     * Method to create a test employee to check the login system before
+     * we use XML parsing for that.
+     */
+    public void createTestEmployee(){
+        String userName = "Manager";
+        String password = "password";
+        boolean hasAdminAccess = true;
+        Employee newEmployee = new Employee(userName, password, hasAdminAccess);
+        addEmployee(newEmployee);
+    }
+}
