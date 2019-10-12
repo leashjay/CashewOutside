391cae30661bda3ff3fc5f1b22fa98f0f9f5a445
Jack Walsh <jwa227@uclive.ac.nz>
2019-10-12 18:06:29 +1300

    Added add ingredient method to the add menu item screen handler and resolved issues in fxml parsing

--- src/main/java/seng202/team3/controller/ManuallyAddMenuItemController.java
+++ src/main/java/seng202/team3/controller/ManuallyAddMenuItemController.java
@@ -5,15 +5,18 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import org.controlsfx.control.textfield.TextFields;
 import javafx.scene.text.Text;
+import seng202.team3.model.Ingredient;
 import seng202.team3.model.Inventory;
 import seng202.team3.model.Menu;
 import seng202.team3.util.InputValidationHelper;
 import seng202.team3.util.ItemType;
 import seng202.team3.view.BusinessApp;
 
+import javax.xml.bind.annotation.XmlElement;
+import java.util.HashMap;
 import java.util.Set;
 
-public class ManuallyAddMenuITemController {
+public class ManuallyAddMenuItemController {
 
     @FXML
     private TextField idTextField;
@@ -51,15 +54,30 @@ public class ManuallyAddMenuITemController {
     @FXML
     private ChoiceBox<ItemType> itemTypeCheckBox;
 
-    /**current inventory for the business */
-    Inventory truckInventory = BusinessApp.getBusiness().getTruck().getInventory();
+    private
 
-    /** current menu */
+    /**
+     * current inventory of ingredients for the business and its corresponding
+     * hash map storing it all
+     */
+    Inventory truckInventory = BusinessApp.getBusiness().getTruck().getInventory();
+    HashMap<String, Ingredient> stock;
+
+    /**
+     * current menu
+     */
     Menu currentMenu = BusinessApp.getBusiness().getMenuManager();
+
+    /**
+     * List of ingredients and their quantities needed to make the menu item
+     */
+    private HashMap<Ingredient, Float> ingredients = new HashMap<>();
+
 
     public void initialize() {
         Set<String> possibleKeys = truckInventory.getIngredients().keySet();
         TextFields.bindAutoCompletion(ingredientKey, possibleKeys);
+
         itemTypeCheckBox.getItems().add(ItemType.BEVERAGE);
         itemTypeCheckBox.getItems().add(ItemType.MAIN);
         itemTypeCheckBox.getItems().add(ItemType.ASIAN);
@@ -67,7 +85,33 @@ public class ManuallyAddMenuITemController {
         itemTypeCheckBox.getItems().add(ItemType.COCKTAIL);
         itemTypeCheckBox.getItems().add(ItemType.OTHER);
         itemTypeCheckBox.getItems().add(ItemType.SNACK);
+
+        markupPercent.setText("120");
     }
+
+    public void updateUnitText() {
+
+    }
+
+    public void addIngredient() {
+        try {
+            float quantity = Float.parseFloat(ingredientQuantity.getText());
+        }
+        catch (NumberFormatException nfe)
+        {
+            ingredientQuantityErrorText.setVisible(true);
+        }
+
+        String id = ingredientKey.getText();
+        Ingredient ingredient;
+        if (stock.containsKey(id)) {
+            ingredient = stock.get(id);
+            // TODO: either seperate the ui scroll pane of ingredient and hash map or have them seperated
+        } else {
+            ingredientKeyErrorText.setVisible(true);
+        }
+    }
+
 
     private boolean checkForErrors() {
         boolean hasError = false;
@@ -96,7 +140,6 @@ public class ManuallyAddMenuITemController {
             String id = idTextField.getText();
             String name = menuItemNameTextField.getText();
             ItemType itemType = itemTypeCheckBox.getValue();
-
         }
 
     }
