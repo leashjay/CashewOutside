fb071e96c930756b31222979e106e70da196dc5e
Hunter Craig <hjc94@uclive.ac.nz>
2019-10-13 22:41:18 +1300

    Made getSalePrice more accurate, so it can be used instead of calculateSalePrice...

--- src/main/java/seng202/team3/model/MenuItem.java
+++ src/main/java/seng202/team3/model/MenuItem.java
@@ -147,15 +147,21 @@ public class MenuItem {
      */
     public float calculateSalePrice() {
         totalCost = getCostPriceFromIngredients();
-        return totalCost * markup;
+        // round the sale price to 2dp
+        return (float) ((Math.round(totalCost * markup)) / 100.0);
     }
 
     /**
+     * ^^ Why do we have two methods that serve such a similar purpose in how they're called? No matter, should be fine
      * Returns the sales price of an item the business sells
      * @return the sales price of a menu item
      */
     public float getSalePrice(){
         // round the sale price to 2 dp
+        if (salePrice == 0) {
+            // when the salePrice is uninitialised, set it properly
+            salePrice = calculateSalePrice();
+        }
         salePrice = (float) (Math.round(salePrice * 100.0) / 100.0);
         return salePrice;
     }
